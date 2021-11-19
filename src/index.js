dd8da6718f5e3f84dde2c77324fc8a87d6468e4e
Michael Netlight <jasoness@yandex.ru>
2021-11-19 17:48:39 +0300

    Fix commit.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Store from './store.js';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import Store from "./store.js";
+import App from "./app.js";
 
 const root = document.getElementById("app");
 
 // Состояние приложения
 const store = new Store({
   items: [
-    {code: 1, title: 'Название элемента'},
-    {code: 2, title: 'Некий объект'},
-    {code: 3, title: 'Заголовок'},
-    {code: 4, title: 'Короткое название'},
-    {code: 5, title: 'Запись'},
-    {code: 6, title: 'Пример названия'},
-    {code: 7, title: 'Седьмой'}
-  ]
+    { code: 1, title: "Название элемента", highlight: 0 },
+    { code: 2, title: "Некий объект", highlight: 14 },
+    { code: 3, title: "Заголовок", highlight: 5 },
+    { code: 4, title: "Короткое название", highlight: 1 },
+    { code: 5, title: "Запись", highlight: 0 },
+    { code: 6, title: "Пример названия", highlight: ''},
+    { code: 7, title: "Седьмой", highlight: 298 },
+  ],
 });
 
 // Сообщаем реакту что и куда рендерить.
 store.subscribe(() => {
-  ReactDOM.render(<App store={store}/>, root);
+  ReactDOM.render(<App store={store} />, root);
 });
 
 // Сообщаем реакту что и куда рендерить.
-ReactDOM.render(<App store={store}/>, root);
+ReactDOM.render(<App store={store} />, root);
